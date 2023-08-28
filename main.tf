e4aa0dec6de7ad2bae73770535fdb7d60a708c87
Olugbenga Ogunsakin <sakin4life@googlemail.com>
2023-08-28 09:57:50 +0100

    update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,41 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnets" "default" {
+    
+}
+
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                     #!/bin/bash
                     echo "Hello, World" > index.html
                     nohup busybox httpd -f -p ${var.server_port} &
                     EOF
-
-    tags = {
-        Name = "terraform-example"
+    lifecycle {
+      create_before_destroy = true
     }
+}
+resource "aws_autoscaling_group"  "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier = data.aws_subnets.default.ids
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+      key = "Name"
+      value = "terraform-asg-example"
+      propagate_at_launch = true
+    }
+  
 }
 
 variable "server_port" {
@@ -34,8 +55,5 @@ resource "aws_security_group" "instance" {
     }
 }
 
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
-}
+
 
